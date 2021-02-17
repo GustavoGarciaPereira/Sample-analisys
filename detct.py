a031df0d9ce1c6b5268800429d6e801b0b488aeb
horberlan <46747395+horberlan@users.noreply.github.com>
2021-02-17 20:05:49 -0300

    Update detct.py

--- detct.py
+++ detct.py
@@ -1,46 +1,41 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %% [markdown]
-#  <h1 align="center">   Projeto: Detectção de celulas e contagem de unidades</h1>
-# 
-# ### Autor: Horberlan Brito
 
-# %%
+# Autor: Horberlan Brito
+
+
 import numpy as np  
 import cv2
 import math
 import matplotlib.pyplot as plt
 
 
-# %%
 img = cv2.imread('drops6.png')
 ############################################
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray, cmap='gray'),plt.title('ORIGINAL')
 
 
-# %%
+
 blur = cv2.GaussianBlur(gray, (11,11),0)
 plt.imshow(blur, cmap='gray')
 
 
-# %%
+
 canny = cv2.Canny(blur, 1,100, 1)
 plt.imshow(canny, cmap='gray')
 
 
-# %%
+
 dilated = cv2.dilate(canny, (1,1), iterations = 2) #intencidade da
 plt.imshow(dilated, cmap='gray')
 
 
-# %%
+
 (cnt, heirarchy) = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 contorno = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #"COLOR_BGR2RGB" Devolve a cor normal a imagem. 
 cv2.drawContours(contorno,cnt, -1, (1,300,1),2) #Cor do contorno
 plt.imshow(contorno),plt.title('Realizando a Contagem de Células')
 
 
-# %%
+
 print(f'Living cells present in the analysis≅: {len(cnt)}')
 
