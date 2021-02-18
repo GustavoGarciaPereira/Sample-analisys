a77a831714feae141477609a98e7f1c9c3d3f7ea
horberlan <46747395+horberlan@users.noreply.github.com>
2021-02-17 22:57:58 -0300

    Update detct.py

--- detct.py
+++ detct.py
@@ -1,4 +1,3 @@
-
 # Autor: Horberlan Brito
 
 
@@ -9,25 +8,20 @@ import matplotlib.pyplot as plt
 
 
 img = cv2.imread('drops6.png')
-############################################
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray, cmap='gray'),plt.title('ORIGINAL')
-
 
 
 blur = cv2.GaussianBlur(gray, (11,11),0)
 plt.imshow(blur, cmap='gray')
 
 
-
 canny = cv2.Canny(blur, 1,100, 1)
 plt.imshow(canny, cmap='gray')
 
 
-
 dilated = cv2.dilate(canny, (1,1), iterations = 2) #intencidade da
 plt.imshow(dilated, cmap='gray')
-
 
 
 (cnt, heirarchy) = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
